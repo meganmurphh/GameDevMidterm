7cb8dedede15931d7542ae6591f4370e164d9ac0
Megan Murphy <mlm4hb@umsystem.edu>
2024-10-21 23:23:14 -0500

    bullet text appearing on the screen

--- Midterm/Assets/Scripts/KeyPressed.cs
+++ Midterm/Assets/Scripts/KeyPressed.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyPressed : MonoBehaviour
 {
@@ -6,8 +7,11 @@ public class KeyPressed : MonoBehaviour
     public GameObject dKey; // Reference to the D key GameObject
     public GameObject fKey; // Reference to the F key GameObject
 
-    public GameObject spherePrefab; // Reference to the sphere prefab
+    public GameObject bullet; // Reference to the sphere prefab
     public Transform cannonArm; // Reference to the cannon arm's position
+
+    public GameObject bulletValueText;
+    public Canvas bulletTextCanvas;
 
     public float shootForce = 500f; // Force with which the sphere will be shot
 
@@ -33,7 +37,8 @@ public class KeyPressed : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S)) // Change to GetKeyDown
         {
             ChangeColor(sKey, lightGreyColor);
-            ShootSphere();
+            ShootSphere(1);
+            Debug.Log("Number 1 shot");
         }
         else
         {
@@ -44,7 +49,9 @@ public class KeyPressed : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D)) // Change to GetKeyDown
         {
             ChangeColor(dKey, lightGreyColor);
-            ShootSphere();
+            ShootSphere(2);
+            Debug.Log("Number 2 shot");
+
         }
         else
         {
@@ -55,7 +62,9 @@ public class KeyPressed : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F)) // Change to GetKeyDown
         {
             ChangeColor(fKey, lightGreyColor);
-            ShootSphere();
+            ShootSphere(3);
+            Debug.Log("Number 3 shot");
+
         }
         else
         {
@@ -70,20 +79,47 @@ public class KeyPressed : MonoBehaviour
     }
 
     // Function to shoot a sphere from the cannon arm
-    private void ShootSphere()
+    private void ShootSphere(int keyNumber)
     {
         // Calculate the spawn position based on the cannon arm's position and its rotation
         Vector3 spawnPosition = cannonArm.position + cannonArm.forward * 1.3f + cannonArm.up * 1.3f;
 
         // Instantiate the sphere at the calculated position
-        GameObject sphere = Instantiate(spherePrefab, spawnPosition, cannonArm.rotation);
+        GameObject bulletObject = Instantiate(bullet, spawnPosition, cannonArm.rotation);
 
         // Add force to the sphere's Rigidbody to shoot it upwards
-        Rigidbody rb = sphere.GetComponent<Rigidbody>();
+        Rigidbody rb = bulletObject.GetComponent<Rigidbody>();
         if (rb != null)
         {
             rb.isKinematic = false; // Make sure it's not kinematic
             rb.AddForce(cannonArm.up * shootForce); // Shoot it straight up
         }
+
+        DisplayValueText(keyNumber, bulletObject.transform);
+
+    }
+
+    // Function to display legacy text
+    private void DisplayValueText(int keyNumber, Transform sphereTransform)
+    {
+        // Instantiate the legacy text prefab at the position above the bullet
+        GameObject textObject = Instantiate(bulletValueText, sphereTransform.position + Vector3.up * 1.5f, Quaternion.identity);
+
+        // Get the Text component from the instantiated object
+        Text numberUIText = textObject.GetComponent<Text>();
+        numberUIText.text = keyNumber.ToString(); // Set the text based on the key number
+
+        // Set the text as a child of the World Space Canvas
+        if (bulletTextCanvas != null)
+        {
+            textObject.transform.SetParent(bulletTextCanvas.transform, false); // Parent to the bullet text canvas
+        }
+        else
+        {
+            Debug.LogError("Bullet Text Canvas is not assigned in the Inspector.");
+        }
+
+        // Position adjustment to make sure text appears correctly above the sphere
+        textObject.transform.position = sphereTransform.position + Vector3.up * 1.5f; // Adjust height as necessary
     }
 }
