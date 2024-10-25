15701f111062aae5c7429a1a4e42e7c1ef8320fe
Megan Murphy <mlm4hb@umsystem.edu>
2024-10-25 00:38:32 -0500

    timer and slider working

---
+++ Midterm/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timerSlider;
+    public Text timerText;
+    public float totalTime = 120f;
+
+    private float remainingTime;
+
+    void Start()
+    {
+        remainingTime = totalTime;
+        timerSlider.maxValue = totalTime;
+        timerSlider.value = totalTime;
+    }
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            timerSlider.value = remainingTime;
+
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            if (remainingTime <= 0)
+            {
+                remainingTime = 0;
+            }
+        }
+    }
+}
