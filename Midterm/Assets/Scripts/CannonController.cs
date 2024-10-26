339063a9afb3bf1c4ed6137aa5f3aefbd0159b99
Megan Murphy <mlm4hb@umsystem.edu>
2024-10-26 12:20:45 -0500

    refactoring

--- Midterm/Assets/Scripts/CannonController.cs
+++ Midterm/Assets/Scripts/CannonController.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 public class CannonController : MonoBehaviour
 {
     public float rotationSpeed = 3f;
-    public float minAngle = -80f;
-    public float maxAngle = 80f;  
+    public float minAngle = -270f;
+    public float maxAngle = 270f;  
 
-    private float currentAngle = 0f; 
+    private StartScreenManager startScreenManager;
+
+    private float currentAngle = 0f;
+
+    private void Start()
+    {
+        startScreenManager = Camera.main.GetComponent<StartScreenManager>();
+    }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-
-        currentAngle -= mouseX * rotationSpeed;
-
-        currentAngle = Mathf.Clamp(currentAngle, minAngle, maxAngle);
-
-        transform.localEulerAngles = new Vector3(0, 0, currentAngle);
+        if (startScreenManager != null && !startScreenManager.IsStartScreenActive)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            currentAngle -= mouseX * rotationSpeed;
+            currentAngle = Mathf.Clamp(currentAngle, minAngle, maxAngle);
+            transform.localEulerAngles = new Vector3(0, 0, currentAngle);
+        }
     }
 }
--- Midterm/Assets/Scripts/StartScreenManager.cs
+++ Midterm/Assets/Scripts/StartScreenManager.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class StartScreenManager : MonoBehaviour
 {
-    public GameObject startScreenCanvas; // Reference to the Start Screen Canvas
+    public GameObject startScreenCanvas;
+    public GameObject pauseMenuCanvas;
+    public bool IsStartScreenActive => startScreenCanvas.activeSelf;
+
+    private bool isPaused = false;
 
     void Start()
     {
@@ -15,5 +18,23 @@ public class StartScreenManager : MonoBehaviour
     {
         startScreenCanvas.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    public void TogglePauseGame()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0f : 1f;
+
+        if (pauseMenuCanvas != null)
+        {
+            pauseMenuCanvas.SetActive(isPaused);
+        }
+
+        Debug.Log(isPaused ? "Game Paused" : "Game Resumed");
+    }
+
+    public void ShowStartScreen()
+    {
+        startScreenCanvas.SetActive(true);
     }
 }
